cfa6adfe2f415d27d3ac44a225315c3c94a4b617
Mahib Abrar <159714107+MahibAbrar@users.noreply.github.com>
2024-12-16 09:53:38 +0000

    c

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from dnslib import DNSRecord
 import socket
 import socketserver
+from flask import Flask, request, render_template_string
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template_string("<h1>Running</h1>")
 
 class DNSHandler(socketserver.BaseRequestHandler):
     def handle(self):
@@ -16,7 +22,9 @@ class DNSHandler(socketserver.BaseRequestHandler):
         except Exception as e:
             print(f"Exception is: {e}")
 if __name__ == "__main__":
-    socketserver.UDPServer.allow_reuse_port = True
-    server = socketserver.UDPServer(("0.0.0.0", 53), DNSHandler)
+    socketserver.ThreadingUDPServer.allow_reuse_port = True
+    server = socketserver.ThreadingUDPServer(("0.0.0.0", 53), DNSHandler)
     print("Running at 53")
     server.serve_forever()
+
+    app.run(host="0.0.0.0", port=80)
